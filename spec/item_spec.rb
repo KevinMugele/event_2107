10abc229da9f09329fc85c4ef29c5ca1027a1ff7
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-02 15:04:50 -0400

    Write code and tests to check initialize

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,2 +1,10 @@
 require './lib/item'
 require './lib/food_truck'
+
+RSpec.describe Item do
+  it "exists" do
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    expect(item1).to be_a Item 
+  end
+end
