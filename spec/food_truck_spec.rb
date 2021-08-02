b05a40ce71feea54c99c9b5c58524bbd6facf22c
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-02 15:08:41 -0400

    Write test and codes for FoodTruck initialize

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,2 +1,17 @@
 require './lib/item'
 require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  it "exists" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck).to be_a FoodTruck
+  end
+
+  it "has a name" do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+  end
+
+end
