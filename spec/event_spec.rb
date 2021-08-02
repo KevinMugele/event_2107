92a7e4fb36f7fe5d250bc13c3a17e56170c3ddac
Kevin Mugele <kevin.mugele@gmail.com>
2021-08-02 15:23:24 -0400

    Write test and code to initialize event

---
+++ spec/event_spec.rb
@@ -0,0 +1,17 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+RSpec.describe Event do
+  it "exists" do
+    event = Event.new("South Pearl Street Farmers Market")
+
+    expect(event).to be_a Event
+  end
+
+  it "has a name" do
+    event = Event.new("South Pearl Street Farmers Market")
+
+    expect(event.name).to eq("South Pearl Street Farmers Market")
+  end
+end
